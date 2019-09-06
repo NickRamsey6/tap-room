c1f9c577bb0e3385d0b899573829fc96ca0e4ab2
Nick Ramsey <nramseysc@gmail.com>
2019-09-06 15:56:47 -0700

    add beer images

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import rainierBeer from '../assets/images/rainierbeer.png'
 
 function Beer(props){
   return (
     <div>
-      <h3>{props.name}</h3>
-      <h3>{props.brand}</h3>
-      <h3>{props.price}</h3>
-      <h3>{props.alcoholContent}</h3>
+      <h3>Beer name: {props.name}</h3>
+      <h3>Beer brand: {props.brand}</h3>
+      <h3>Beer price: ${props.price}</h3>
+      <h3>Alcohol Content: {props.alcoholContent}</h3>
+      <img src={rainierBeer}/>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(){
   return (
     <div>
       <h1>Tap Room</h1>
+      <Link to ="/">Home</Link> | <Link to="/newbeer">Add a Beer</Link>
     </div>
   );
 }
