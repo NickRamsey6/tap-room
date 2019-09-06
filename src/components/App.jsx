0dbfc84ae553dfa868695dbf13036c79c301eafb
Nick Ramsey <nramseysc@gmail.com>
2019-09-06 13:53:57 -0700

    error404 routing for non existent paths

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import BeerList from './BeerList';
 import { Switch, Route } from 'react-router-dom';
+import Error404 from './Error404';
 
 function App(){
   return (
@@ -9,6 +10,7 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={BeerList} />
+        <Route component={Error404} />
       </Switch>
     </div>
   );
