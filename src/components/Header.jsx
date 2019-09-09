152a61190a723c1976f8562576344c0ed739b119
Nick Ramsey <nramseysc@gmail.com>
2019-09-08 19:49:27 -0700

    header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/images/cnfLogo.png'
 
 function Header(){
   return (
     <div>
-      <h1>Tap Room</h1>
-      <Link to ="/">Home</Link> | <Link to="/newbeer">Add a Beer</Link>
+      <div className="header">
+      <h3>$3 Pint Late Night Happy Hour 9-11 pm</h3>
+      </div>
+      <style jsx>{`
+          .header {
+            background-color: #7A9CBF;
+            text-align: center;
+            width: 100%;
+            top: 0;
+            left: 0;
+            position: absolute;
+          }
+          `}</style>
+        <div className="logo">
+          <img src={logo}/>
+        </div>
+        <style jsx>{`
+            .logo {
+              padding-top: 80px;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;
+              width: 40%;
+            }
+            `}</style>
+          <div className="link-bar">
+          <Link to ="/">HOME</Link> | <Link to="/newbeer">ADD A BEER</Link> | <Link to="/aboutUs"> ABOUT US</Link> | <Link to="/tapList"> LIVE TAP LIST</Link> | <Link to="/events"> UPCOMING EVENTS</Link> | <Link to="/recommend"> RECOMMEND!</Link> | <Link to="/kegSales"> KEG SALES</Link>
+            <style jsx>{`
+                .link-bar {
+                  text-align: center;
+                }
+                `}</style>
+          </div>
     </div>
   );
 }
